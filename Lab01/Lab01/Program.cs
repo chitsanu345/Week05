04a3642a09573eb0b65c36f374f35eb141a8f19c
chitsanu345 <chitsanu345@gmail.com>
2017-09-08 11:46:36 +0700

    P14

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,20 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            if (a == 10)
+            {
+                if (b == 20)
+                {
+                    Console.WriteLine("a = 10 and b = 20");
+                }
+                if (b != 20)
+                {
+                    Console.WriteLine("a = 10 and b != 20");
+                }
+            }
+
         }
     }
 }
