54afce7cb10d6bdcd8fcb469da935b57837fef6d
chitsanu345 <chitsanu345@gmail.com>
2017-09-08 11:37:07 +0700

    P13

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,18 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int a = 2;
+            if (a == 2)
+            {
+                Console.WriteLine("execute this line");
+            }
+            else
+            {
+                Console.WriteLine("execute another line too");
+            }
+            Console.WriteLine("this line is always execute");
+
+
         }
     }
 }
