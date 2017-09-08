997055120579b049d98bb330a956ce6dbdc6e532
chitsanu345 <chitsanu345@gmail.com>
2017-09-08 10:16:53 +0700

    P05

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,17 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            object o2 = null;
+            try
+            {
+                int i2 = (int)o2;
+                Console.WriteLine("i2 = {0}", i2);
+            }
+            catch
+            {
+                Console.WriteLine("Error, null object assignment.");
+            }
+
         }
     }
 }
