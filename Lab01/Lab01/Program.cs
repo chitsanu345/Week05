3fc36cbdd83383bea7228244b12d34f138818340
chitsanu345 <chitsanu345@gmail.com>
2017-09-08 11:16:41 +0700

    P10

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,14 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            int c = add(a, b);
+        }
+        private static int add(int a, int b)
+        {
+            throw new NotImplementedException();
+
         }
     }
 }
