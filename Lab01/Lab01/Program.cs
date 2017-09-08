96b0af592fa16a08c6feb1b6fc5deb4cdbbd4f8a
chitsanu345 <chitsanu345@gmail.com>
2017-09-08 11:26:07 +0700

    P11

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,34 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            int c;
+            try
+            {
+                c = div(a, b);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("DivideByZeroException");
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("NullReferenceException");
+                Console.WriteLine(e.Message);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception");
+                Console.WriteLine(e.Message);
+            }
+        }
+        private static int div(int a, int b)
+        {
+            throw new FileNotFoundException();
+
         }
     }
 }
