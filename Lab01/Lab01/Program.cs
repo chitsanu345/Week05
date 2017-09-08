07ad6b76e456eac660cba9591ebeb1adddba21fe
chitsanu345 <chitsanu345@gmail.com>
2017-09-08 10:43:49 +0700

    P07

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int a = int.MaxValue;
+            a *= 2;
+            Console.WriteLine(a);
+
         }
     }
 }
