ba590f126f7bcb40cc1f7365a0be2b655c006b7f
chitsanu345 <chitsanu345@gmail.com>
2017-09-14 22:57:09 +0700

    P22

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,15 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 12; i++)
+            {
+                for (int j = 1; j <= 12; j++)
+                {
+                    Console.Write("{0, 4}", i * j);
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
