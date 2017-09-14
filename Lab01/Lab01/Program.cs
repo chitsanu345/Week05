4aee7a55b64ddecdd657dad56f2d5dac90009712
chitsanu345 <chitsanu345@gmail.com>
2017-09-14 23:05:33 +0700

    P25

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,29 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your selection (1, 2, or 3): ");
+            string s = Console.ReadLine();
+            int n = Int32.Parse(s);
+
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Current value is {0}", 1);
+                    break;
+                case 2:
+                    Console.WriteLine("Current value is {0}", 2);
+                    break;
+                case 3:
+                    Console.WriteLine("Current value is {0}", 3);
+                    break;
+                default:
+                    Console.WriteLine("Sorry, invalid selection.");
+                    break;
+            }
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
