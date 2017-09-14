542ef1b60427305c78cf941569298709907001da
chitsanu345 <chitsanu345@gmail.com>
2017-09-14 23:03:02 +0700

    P24

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,19 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }
