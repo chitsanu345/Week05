c85181fcb107b8df480240faaa2d7890b0c430af
chitsanu345 <chitsanu345@gmail.com>
2017-09-14 22:04:10 +0700

    P16

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,32 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            Console.Write("Input your grade (A, B, C, D or F) : ");
+            string gradeString = Console.ReadLine();
+            string message;
+            switch (gradeString.ToUpper())
+            {
+                case "A":
+                    message = "Excellent";
+                    break;
+                case "B":
+                    message = "Good";
+                    break;
+                case "C":
+                    message = "Cool";
+                    break;
+                case "D":
+                    message = "Try";
+                    break;
+                case "F":
+                    message = "Get out!!";
+                    break;
+                default:
+                    message = "Incorrect grade";
+                    break;
+            }
+            Console.WriteLine(message);
+
         }
     }
 }
