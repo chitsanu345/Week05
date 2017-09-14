36d8e324558d3ff7b0a1d2f77aeb2d6ba87129c9
chitsanu345 <chitsanu345@gmail.com>
2017-09-14 21:45:59 +0700

    P15

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,17 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int point = 68;
+            if (point < 50)
+                Console.WriteLine("Grade F");
+            else if (point < 60)
+                Console.WriteLine("Grade D");
+            else if (point < 70)
+                Console.WriteLine("Grade C");
+            else if (point < 80)
+                Console.WriteLine("Grade B");
+            else
+                Console.WriteLine("Grade A");
         }
     }
 }
